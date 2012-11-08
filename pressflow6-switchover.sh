aa87663c195749c85a58fbd7809bbdd0f72807c3
Chris Hales <chris.hales@medaicurrent.com>
2012-11-08 00:58:52 -0500

    minor comment edits

--- pressflow6-switchover.sh
+++ pressflow6-switchover.sh
@@ -10,18 +10,18 @@
 #
 # Run this script from within the Drupal root that you wish to update.
 # $ cp /path/to/pressflow-switch.sh /path/to/drupal
-# $ ./path/to/drupal/pressflow-switch.sh
+# $ ./path/to/drupal/pressflow6-switchover.sh
 #
 # NOTE: Please backup your site prior to running this script just in case and
 # be sure to adjust the skips as needed. Comment out any files you may have
 # customized, you will need to merge your changes manually for those.
 
-# Download and unpack the latest Pressflow from Github.
+# Download and unpack the latest Pressflow archive from Github.
 curl -C - -o master.tar.gz https://nodeload.github.com/pressflow/6/tar.gz/master
 tar -zxvf master.tar.gz
 
-# Use rsync to sync files and remove extras that should be orphaned. Exclude
-# .svn directories in case we are under version control.
+# Use rsync to sync files and remove extras that would be orphaned. Also exclude
+# .svn directories in case we are under SVN version control.
 mv pressflow/*.txt ./
 mv pressflow/*.php ./
 rsync -avzpP --delete --exclude '.svn' 6-master/includes ./
@@ -38,7 +38,5 @@ rsync -avzpP --delete --exclude '.svn' 6-master/themes ./
 # Cleanup
 rm -r 6-master master.tar.gz
 
-# If you have drush installed you may want to run the database update and cache
-# clear commands.
+# If you have drush installed you may want to run the database update command.
 #drush updatedb -y
-#drush cache-clear all
