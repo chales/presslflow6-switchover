8b0d2e6463fa3fbf3d17e44a69b7d209b11b6ac2
Chris Hales <chris.hales@mediacurrent.com>
2013-08-06 23:38:17 -0400

    Update pressflow6-switchover.sh
    Changes to specify all files individually instead of *.php and *.txt files.

--- pressflow6-switchover.sh
+++ pressflow6-switchover.sh
@@ -3,11 +3,11 @@
 # NOTE: USE AT YOUR OWN RISK, this script will overwrite your Drupal install.
 #
 # This bash script will allow you to overwrite you current core Drupal files with
-# the latest Pressflow release in a sane manner.
+# the latest Pressflow 6.x release in a sane manner.
 #
 # Please backup your site prior to running this script just in case and
-# be sure to adjust the skips as needed. Be sure to move any files you may have
-# customized, you will need to merge your changes back manually.
+# be sure to adjust the skips as needed. You will need to merge your
+# customizations back in manually otherwise.
 #
 # Run this script from within the Drupal root that you wish to update.
 # $ cp /path/to/pressflow6-switchover.sh /path/to/drupal
@@ -26,20 +26,32 @@
 curl -Lk -o master.zip https://github.com/pressflow/6/archive/master.zip
 unzip master.zip
 
+# Skip any customized files by commenting them out such as .htaccess or your
+# robots.txt file.
+#mv 6-master/robots.txt ./
+#mv 6-master/.htaccess ./
+mv 6-master/CHANGELOG.txt ./
+mv 6-master/COPYRIGHT.txt ./
+mv 6-master/cron.php ./
+mv 6-master/index.php ./
+mv 6-master/INSTALL.mysql.txt ./
+mv 6-master/INSTALL.pgsql.txt ./
+mv 6-master/install.php ./
+mv 6-master/INSTALL.txt ./
+mv 6-master/LICENSE.txt ./
+mv 6-master/MAINTAINERS.txt ./
+mv 6-master/update.php ./
+mv 6-master/UPGRADE.txt ./
+mv 6-master/xmlrpc.php ./
+
 # Use rsync to sync files and remove extras that would be orphaned. Exclude
 # .svn directories in case we are under SVN version control.
-# To Do - work it out so that robots.txt can be skipped easily.
-mv 6-master/*.txt ./
-mv 6-master/*.php ./
 rsync -avzpP --delete --exclude '.svn' 6-master/includes ./
 rsync -avzpP --delete --exclude '.svn' 6-master/misc ./
 rsync -avzpP --delete --exclude '.svn' 6-master/modules ./
 rsync -avzpP --delete --exclude '.svn' 6-master/profiles ./
 rsync -avzpP --delete --exclude '.svn' 6-master/scripts ./
 rsync -avzpP --delete --exclude '.svn' 6-master/themes ./
-
-# Skip any customized files. You certainly don't want to overwrite /sites
-#mv pressflow/.htaccess ./
 #rsync -avzpP 6-master/sites ./
 
 # Cleanup
